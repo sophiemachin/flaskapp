81885b07b1439aa1358f8af61a6fe0da6b25e190
Sophie Machin <sophiemachin@gmail.com>
2019-07-31 17:18:56 +0100

    app: add count words function

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+import string
+from collections import Counter
 import flask
 from flask_cors import cross_origin, CORS
 
@@ -14,6 +16,13 @@ def hello_world():
 @app.route('/route1')
 def route1():
     return 'This is route1'
+
+
+def count_words(s):
+    punc_to_remove = string.punctuation.translate({ord('-'): None})
+    no_punc = s.translate(str.maketrans('', '', punc_to_remove))
+    lower = no_punc.lower()
+    return Counter(lower.split())
 
 
 @app.route('/json', methods=['GET', 'POST'])
