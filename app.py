689880c3f6c399d02aec805f749535ea934289a9
Sophie Machin <sophiemachin@gmail.com>
2019-07-31 16:12:00 +0100

    app: get request data

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import flask
 from flask_cors import cross_origin, CORS
 
@@ -16,9 +16,10 @@ def route1():
     return 'This is route1'
 
 
-@app.route('/json', methods=['GET'])
+@app.route('/json', methods=['GET', 'POST'])
 @cross_origin(origin='*')
 def json():
+    print(request.data)
     data = {'somekey': 'somevalue'}
     return flask.jsonify(data), 200
 
