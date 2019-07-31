b51116a10cdad1e67502446aeb8d0f2ff137db43
Sophie Machin <sophiemachin@gmail.com>
2019-07-31 10:39:54 +0100

    app: get cross-origin working

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import flask
+from flask_cors import cross_origin
 
 app = Flask(__name__)
 
@@ -11,6 +13,13 @@ def hello_world():
 @app.route('/route1')
 def route1():
     return 'This is route1'
+
+
+@app.route('/json', methods=['GET'])
+@cross_origin(origin='*')
+def json():
+    data = {'somekey': 'somevalue'}
+    return flask.jsonify(data), 200
 
 
 if __name__ == '__main__':
